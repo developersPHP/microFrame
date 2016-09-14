177d6411645e20c6f08b29b8136bada70880132c
David.Liu <15219740206@163.com>
2016-09-14 23:08:37 +0800

    Update test.php

--- app/Action/test.php
+++ app/Action/test.php
@@ -1,2 +1,16 @@
 <?php
-this is a test
+<?php
+namespace App\Action;
+define('L_APP_PATH',dirname(__DIR__).'/');
+define('L_WORKSPACE_PATH',dirname(L_APP_PATH) . '/');
+
+include_once L_WORKSPACE_PATH.'common/showClass.php';
+use microFrame\common\showClass;
+class test extends \microFrame\lavender\BaseClass
+{
+    public function test1_action() {
+        $report = array(11);
+        $show = new showClass();
+        return $show->success('这是一个测试',$report);
+    }
+}
