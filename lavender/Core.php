08269123299ae2cd3080eb615f3148907397f638
David.Liu <15219740206@163.com>
2016-09-18 11:46:42 +0800

    Update Core.php
    add a response function and skip to error page

--- lavender/Core.php
+++ lavender/Core.php
@@ -323,4 +323,65 @@ final class Core
             'action' => empty($route_info[1]) ? '' : trim($route_info[1]),
         );
     }
+    
+    //the function is used to respond error status
+    public static function header_status($code, $replace = true)
+    {
+        $headers = array(
+            100 => 'Continue',
+            101 => 'Switching Protocols',
+
+            200 => 'OK',
+            201 => 'Created',
+            202 => 'Accepted',
+            203 => 'Non-Authoritative Information',
+            204 => 'No Content',
+            205 => 'Reset Content',
+            206 => 'Partial Content',
+
+            300 => 'Multiple Choices',
+            301 => 'Moved Permanently',
+            302 => 'Found',
+            303 => 'See Other',
+            304 => 'Not Modified',
+            305 => 'Use Proxy',
+            307 => 'Temporary Redirect',
+
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            407 => 'Proxy Authentication Required',
+            408 => 'Request Timeout',
+            409 => 'Conflict',
+            410 => 'Gone',
+            411 => 'Length Required',
+            412 => 'Precondition Failed',
+            413 => 'Request Entity Too Large',
+            414 => 'Request-URI Too Long',
+            415 => 'Unsupported Media Type',
+            416 => 'Requested Range Not Satisfiable',
+            417 => 'Expectation Failed',
+
+            500 => 'Internal Server Error',
+            501 => 'Not Implemented',
+            502 => 'Bad Gateway',
+            503 => 'Service Unavailable',
+            504 => 'Gateway Timeout',
+            505 => 'HTTP Version Not Supported'
+        );
+
+        if (!isset($headers[$code] ) ) {
+            return false;
+        }
+
+        if (empty($_SERVER["SERVER_PROTOCOL"]) || ('HTTP/1.1' != $_SERVER["SERVER_PROTOCOL"] && 'HTTP/1.0' != $_SERVER["SERVER_PROTOCOL"]) ) {
+            $_SERVER["SERVER_PROTOCOL"] = 'HTTP/1.0';
+        }
+
+        header("{$_SERVER["SERVER_PROTOCOL"]} {$code} {$headers[$code]}", $replace, $code);
+        return true;
+    }
 }
